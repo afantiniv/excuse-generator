d193367b18cdb3fa86f5553325b5937cf5e8bef1
afantiniv <fantiniv95@gmail.com>
2022-10-17 18:00:58 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function generator() {
+  let x = who[Math.floor(Math.random() * who.length)];
+  let y = what[Math.floor(Math.random() * what.length)];
+  let z = when[Math.floor(Math.random() * when.length)];
+  return x + " " + y + " " + z;
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generator();
+  console.log("Look Mom! I'm on the internet!");
 };
